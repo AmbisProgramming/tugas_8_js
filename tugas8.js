879a1875744103b03694360071ec2126e12224fb
AmbisProgramming <110365940+AmbisProgramming@users.noreply.github.com>
2022-08-01 12:55:56 +0700

    Add files via upload

---
+++ tugas8.js
@@ -0,0 +1,14 @@
+var motor = {
+  merek: "Honda",
+  varian: "Vario",
+  type_cc: 150,
+};
+
+console.log(motor);
+
+//Mengubah dan menambahkan property
+motor.varian = "Vario 2022";
+motor.jenis = "Abs";
+motor.harga = 21_000_00;
+
+console.log(motor);
